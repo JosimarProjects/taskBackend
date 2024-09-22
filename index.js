24ebcf6f3844d200e35e51da0eff9f0dd591d9a5
JosimarProjects <75262276+JosimarProjects@users.noreply.github.com>
2024-09-22 10:39:25 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const db = require('./config/db')
+const consign = require('consign')
+
+consign()
+    .then('./config/middleware.js')    
+    .into(app)
+
+    
+
+app.db = db
+
+
+app.listen(3000, () => {
+    console.log('Listening on port 3000')
+})
---
+++ migrations/20240921132331_create_table_tasks.js
@@ -0,0 +1,25 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+
+    return knex.schema.createTable('tasks', table => {
+        table.increments('id').primary()
+        table.string('desc').notNullable()
+        table.datetime('estimateAt')
+        table.datetime('doneAt')
+        table.integer('userId').references('id').inTable('users').onDelete('CASCADE').notNullable()
+    })
+}
+
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+
+    return knex.schema.dropTable('tasks')
+
+};
