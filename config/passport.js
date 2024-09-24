10451b7802e9633afe69f0888a7b204532db5d6e
JosimarProjects <75262276+JosimarProjects@users.noreply.github.com>
2024-09-24 18:29:37 -0300

    finish back

--- config/passport.js
+++ config/passport.js
@@ -6,22 +6,20 @@ const { Strategy, ExtractJwt } = passportJwt
 module.exports = app => {
     const params = {
         secretOrKey: authSecret,
-        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+        jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     }
-
+    
     const strategy = new Strategy(params, (payload, done) => {
         app.db('users')
             .where({ id: payload.id })
             .first()
             .then(user => {
-                if(user) {
-                    done(null, { id: user.id, name: user.name, email: user.email })
+                if (user) {
+                    done(null, { id: user.id, email: user.email })
                 } else {
                     done(null, false)
                 }
-
-            })              
-                
+            })
             .catch(err => done(err, false))
     })
 
@@ -29,6 +27,6 @@ module.exports = app => {
 
     return {
         initialize: () => passport.initialize(),
-        authenticate: () => passport.authenticate('jwt', { session: false })
+        authenticate: () => passport.authenticate('jwt', { session: false }),
     }
 }
