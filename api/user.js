dd3638f87130d9db7f59c874d74ce9720e1e26af
JosimarProjects <75262276+JosimarProjects@users.noreply.github.com>
2024-10-02 18:58:47 -0300

    user lower case

--- api/user.js
+++ api/user.js
@@ -11,9 +11,13 @@ module.exports = app => {
         obterHash(req.body.password, hash => {
             const password = hash
             app.db('users')
-            .insert({ name: req.body.name, email: req.body.email, password })
-            .then(_ => res.status(204).send())
-            .catch(err => res.status(400).json(err))
+                .insert({
+                    name: req.body.name,
+                    email: req.body.email.toLowerCase(),
+                    password
+                })
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(400).json(err))
         })
     }
 
